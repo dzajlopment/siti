28cd4b42546721c04bbdc01e30ebad543cfcbdee
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-13 17:55:30 +0200

    Install react-navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { Provider as PaperProvider, Text } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
-      <Text>Hello, world</Text>
+      <NavigationContainer>
+        <Text>Hello, world</Text>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
