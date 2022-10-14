c6aab26509675240cc26757917a28634e28df2fb
LordPrinz <plazinski2902@gmail.com>
2022-10-14 17:33:58 +0200

    Screens/Add Map screen

---
+++ screens/Map.tsx
@@ -0,0 +1,14 @@
+import MapView from "react-native-maps";
+
+const Map = () => {
+	const region = {
+		latitude: -37.78,
+		longitude: -122.43,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421,
+	};
+
+	return <MapView initialRegion={region} />;
+};
+
+export default Map;
