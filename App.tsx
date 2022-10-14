5def8e4c11496daab64e4920ee3151e13286aee6
LordPrinz <plazinski2902@gmail.com>
2022-10-14 17:44:42 +0200

    Navigation/Implement basic navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider as PaperProvider, Text } from "react-native-paper";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { Provider as PaperProvider } from "react-native-paper";
 import LocationPicker from "./components/Reports/LocationPicker";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<PaperProvider>
-			<NavigationContainer>
-				<Text>Hello, world</Text>
-				<LocationPicker />
-			</NavigationContainer>
-		</PaperProvider>
+		<>
+			<StatusBar style="dark" />
+			<PaperProvider>
+				<NavigationContainer>
+					<Stack.Navigator>
+						<Stack.Screen name="LocationPicker" component={LocationPicker} />
+					</Stack.Navigator>
+				</NavigationContainer>
+			</PaperProvider>
+		</>
 	);
 }
