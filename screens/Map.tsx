101b91a6d3551898bf835f83a80613a965653d7c
LordPrinz <plazinski2902@gmail.com>
2022-10-14 18:12:51 +0200

    Location/Let user pick location

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,15 +1,42 @@
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { MapEvent, Marker } from "react-native-maps";
+import { useState } from "react";
 
 const Map = () => {
+	const [selectedLocation, setSelectedLocation] = useState<
+		undefined | { lat: number; lng: number }
+	>();
+
 	const region = {
-		latitude: -37.78,
+		latitude: 37.78,
 		longitude: -122.43,
 		latitudeDelta: 0.0922,
 		longitudeDelta: 0.0421,
 	};
 
-	return <MapView style={styles.map} initialRegion={region}></MapView>;
+	const selectLocationHandler = (event: MapEvent) => {
+		const { latitude: lat, longitude: lng } = event.nativeEvent.coordinate;
+		setSelectedLocation({ lat, lng });
+	};
+
+	return (
+		<MapView
+			style={styles.map}
+			initialRegion={region}
+			scrollEnabled={true}
+			onPress={selectLocationHandler}
+		>
+			{selectedLocation && (
+				<Marker
+					title="Picked Location"
+					coordinate={{
+						latitude: selectedLocation.lat,
+						longitude: selectedLocation.lng,
+					}}
+				/>
+			)}
+		</MapView>
+	);
 };
 
 export default Map;
