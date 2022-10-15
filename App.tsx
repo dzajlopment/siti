c681681dbc144e46927b9f82a1cf5330c208bb31
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-15 20:51:21 +0200

    Create screen with list of ideas

--- App.tsx
+++ App.tsx
@@ -1,27 +1,28 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider } from "react-native-paper";
 import LocationPicker from "./components/ReportsForm/LocationPicker";
+import { IdeasListScreen } from "./features/ideas-list/IdeasListScreen";
 import { ReportList } from "./features/reports/ReportList";
 import Map from "./screens/Map";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-	return (
-		<>
-			<StatusBar style="dark" />
-			<PaperProvider>
-				<NavigationContainer>
-					<Stack.Navigator initialRouteName="ReportList">
-						<Stack.Screen name="ReportList" component={ReportList} />
-						<Stack.Screen name="AddPlace" component={LocationPicker} />
-						<Stack.Screen name="Map" component={Map} />
-					</Stack.Navigator>
-				</NavigationContainer>
-			</PaperProvider>
-		</>
-	);
+  return (
+    <>
+      <StatusBar style="dark" />
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="ReportList">
+            <Stack.Screen name="ReportList" component={ReportList} />
+            <Stack.Screen name="AddPlace" component={LocationPicker} />
+            <Stack.Screen name="Map" component={Map} />
+            <Stack.Screen name="IdeaList" component={IdeasListScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </>
+  );
 }
---
+++ features/ideas-list/IdeasListScreen.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Idea } from "../../domain/Idea";
+import { IdeasList } from "./IdeasList";
+
+export const IdeasListScreen = (props: { navigation: any }) => {
+  const navigation = props.navigation;
+  const [ideas, setIdeas] = useState<Idea[]>([]);
+
+  const handleNewIdeaPressed = () => {
+    navigation.push();
+  };
+
+  return (
+    <>
+      <IdeasList ideas={ideas} onNewIdeaPress={handleNewIdeaPressed} />
+    </>
+  );
+};
