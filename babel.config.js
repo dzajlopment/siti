5c1e07d2054be7fb0c847f00dc04fa5539b49579
LordPrinz <plazinski2902@gmail.com>
2022-10-19 21:51:44 +0200

    Configure dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,19 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
-  };
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        env: {
+            production: {
+                plugins: [
+                    "react-native-paper/babel", [
+                        "module:react-native-dotenv",
+                        {
+                            moduleName: "@env",
+                            path: ".env",
+                        },
+                    ],
+                ],
+            },
+        },
+    };
 };
